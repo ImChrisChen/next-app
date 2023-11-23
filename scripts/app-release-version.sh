a96db71579c4d1155def19fa25339d71d89226db
imchrisorz <imchrisorz@gmail.com>
2023-11-23 15:28:17 +0800

    chore: fix app-release-version.sh

--- scripts/app-release-version.sh
+++ scripts/app-release-version.sh
@@ -2,13 +2,11 @@
 
 set -e
 
-git add .
-
 npm version $1  # patch minor major
 
 version=$(node -p "require('./package.json').version")
 
-git commit --amend "chore: app release $version"
+git commit --amend -m "chore: app release $version"
 
 git push origin --tags
 
